889c6b499ae452c40bd4be9cf3a69c32e8d68cf8
syswraith <syswraith@proton.me>
2026-03-08 19:35:37 +0530

    Quartz sync: Mar 8, 2026, 7:35 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,9 +39,9 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true, // whether to allow zooming in and out
         depth: 1, // how many hops of notes to display
         scale: 1.1, // default view scale
-        repelForce: 0.5, // how much nodes should repel each other
+        repelForce: 1.0, // how much nodes should repel each other
         centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
+        linkDistance: 60, // how long should the links be by default?
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
@@ -53,9 +53,9 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true,
         depth: -1,
         scale: 0.9,
-        repelForce: 0.5,
+        repelForce: 1.0,
         centerForce: 0.3,
-        linkDistance: 10,
+        linkDistance: 30,
         fontSize: 0.6,
         opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
